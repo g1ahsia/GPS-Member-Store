9ce33a14a75c3602763d6be5c95691eb5b8673cb
Allen <g1ahsia@gmail.com>
2020-07-24 17:11:09 +0800

    Initial Commit

---
+++ GPS Member Store/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GPS Member Store
+//
+//  Created by Allen Hsiao on 2020/7/24.
+//  Copyright © 2020 Allen Hsiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GPS Member StoreTests/GPS_Member_StoreTests.swift
@@ -0,0 +1,34 @@
+//
+//  GPS_Member_StoreTests.swift
+//  GPS Member StoreTests
+//
+//  Created by Allen Hsiao on 2020/7/24.
+//  Copyright © 2020 Allen Hsiao. All rights reserved.
+//
+
+import XCTest
+@testable import GPS_Member_Store
+
+class GPS_Member_StoreTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
